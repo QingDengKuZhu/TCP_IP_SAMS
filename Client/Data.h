f890363b805f053f0ece23964ceee39a6a44b880
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-04-08 21:39:08 +0800

    修改学生信息

--- Client/Data.h
+++ Client/Data.h
@@ -8,7 +8,7 @@
 
 //学生数据结构
 
-typedef struct stutent
+typedef struct Stutent
 {
 	char ID[10];			//学号位数为9,最后一位存放'\0'.
 	char name[15];		//姓名最多14位.最后一位存放'\0'.
@@ -21,9 +21,8 @@ typedef struct stutent
 	unsigned int biology;	//生物成绩
 
 	unsigned int total;	//总分
-	double	ave;			//平均分
 
-}STUDENT;
+} STUDENT;
 
 
 #endif
