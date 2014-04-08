f738d57464059ee32c4193eb1cc8ec4877054581
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-04-08 17:59:13 +0800

    数据输出

---
+++ Client/Output.h
@@ -0,0 +1,15 @@
+﻿#ifndef OUTPUT_H
+#define OUTPUT_H
+
+/*
+**格式化输出表头,当表格形式输出用户信息时输出表头信息.
+*/
+void PrintHead(void);
+
+/*
+**格式化输出接收缓冲区中的一条信息(一个学生信息)
+*/
+void PrintData(const char *receive_buffer);
+
+
+#endif
