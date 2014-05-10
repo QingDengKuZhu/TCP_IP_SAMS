05bc7e07a4097654131d813491d7f4ca8e472c60
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-05-10 23:15:24 +0800

    验证帐号密码

---
+++ Server/Function.c
@@ -0,0 +1,58 @@
+﻿#include "Data.h"
+#include "Function.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int IsAccount(const long int account, const long int password)
+{
+	ACCOUNT *p = NULL;
+	FILE *pf = NULL;		/*文件指针*/
+	
+	p = (ACCOUNT *)malloc(sizeof(ACCOUNT));
+	if (!p)
+	{
+		printf("头结点分配失败!\n");
+		exit(EXIT_FAILURE);
+	}
+	/*
+	** 以追加的方式打开一个二进制文件,可读可写,若文件不存在,则创建文件
+	*/
+	pf = fopen(PASSWORDPATH, "ab+");//最后必须修改为只读模式
+	if (!pf)
+	{
+		printf("文件打开错误!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	** 读入数据
+	*/
+	while (0 == feof(pf))	/*若文件未读到尾部*/
+	{
+		
+		/*
+		** 一次从文件中读取一条学生记录
+		*/
+		if ( 1 == fread(p, sizeof(NODE), 1, pf))
+		{
+			if (account == p->account )/*帐号存在*/
+			{
+				if (password == p->password)/*密码正确,返回帐号类型*/
+				{
+					return p->Type;
+				}
+				else	/*密码错误*/
+				{
+					return 4;
+				}
+			}
+			else	/*帐号不存在*/
+			{
+				return 5;
+			}
+		}
+	}
+
+	fclose(pf);	/*关闭文件*/
+	return 5;
+}
---
+++ Server/Function.h
@@ -0,0 +1,9 @@
+﻿#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "Data.h"
+
+//判断接收到的帐号密码请求是否正确,若正确返回相应的帐号类型(1或2),若不正确,返回4(密码错误),5(帐号不存在)
+int IsAccount(const long int account, const long int password);
+
+#endif
