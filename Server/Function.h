f356f4335635784ead03c7554fcc24febc80c365
QingDengKuZhu <qingdengkuzhu@foxmail.com>
2014-05-17 11:08:41 +0800

    修改,添加函数声明

--- Server/Function.h
+++ Server/Function.h
@@ -2,13 +2,17 @@
 #define FUNCTION_H
 
 #include "Data.h"
+#include <WinSock2.h>
 
 //判断接收到的帐号密码请求是否正确,若正确返回相应的帐号类型(1或2),若不正确,返回3(密码错误),4(帐号不存在)
 int IsAccount(const char account[], const char password[]);
 
 //当以学生身份登录服务器时,调用此函数.
-void Student(SOCKET hClientSocket);
+void Student(SOCKET hClientSocket, const char account[]);
 
 //当以教师身份登录服务器时,调用此函数.
 void	Teacher(SOCKET hClientSocket);
+
+//删除单链表
+void DestroyList(LINK **ppL);
 #endif
